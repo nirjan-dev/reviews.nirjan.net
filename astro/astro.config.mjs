64637cd344d860da471a8f83698f4f725eb83b18
nirjankhadka <nirjan.khadka@gfinity.net>
2022-12-31 11:21:06 +0545

    feat: add tailwind, accessible-astro-components and header components

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import sanity from "astro-sanity";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sanity-minimal-starter.netlify.app/",
   // Your public domain, e.g.: https://my-site.dev/
-  integrations: [
-    sitemap(),
-    sanity({
-      projectId: "69oav2ia",
-      dataset: "production",
-      apiVersion: "v2021-03-25",
-      useCdn: true,
-    }),
-  ],
+  integrations: [sitemap(), sanity({
+    projectId: "69oav2ia",
+    dataset: "production",
+    apiVersion: "v2021-03-25",
+    useCdn: true
+  }), tailwind()]
 });
