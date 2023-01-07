51ec9696d9aa9e398621055d4cd7f320b749cd94
nirjankhadka <nirjan.khadka@gfinity.net>
2023-01-07 11:46:53 +0545

    feat: finish initial design of the site

--- astro/src/lib/api.js
+++ astro/src/lib/api.js
@@ -6,6 +6,30 @@ export async function getAllPosts() {
   return data;
 }
 
+export async function getAllMusicPosts() {
+  const query = `*[_type == 'post' && type == 'music']{"categoryData": categories[]->{slug, title},author -> {name}, ...} | order(publishedAt desc)`;
+  const data = await useSanityClient().fetch(query);
+  return data;
+}
+
+export async function getAllMoviesPosts() {
+  const query = `*[_type == 'post' && type == 'movies']{"categoryData": categories[]->{slug, title},author -> {name}, ...} | order(publishedAt desc)`;
+  const data = await useSanityClient().fetch(query);
+  return data;
+}
+
+export async function getAllBooksPosts() {
+  const query = `*[_type == 'post' && type == 'books']{"categoryData": categories[]->{slug, title},author -> {name}, ...} | order(publishedAt desc)`;
+  const data = await useSanityClient().fetch(query);
+  return data;
+}
+
+export async function getRecentPosts() {
+  const query = `*[_type == 'post']{"categoryData": categories[]->{slug, title},author -> {name}, ...} | order(publishedAt desc) [0...3]`;
+  const data = await useSanityClient().fetch(query);
+  return data;
+}
+
 export async function getAllCategoriesWithPosts() {
   const query = `*[_type == 'category']{"posts": *[_type == "post" && references(^._id)] | order(publishedAt desc), ...}`;
   const data = await useSanityClient().fetch(query);
